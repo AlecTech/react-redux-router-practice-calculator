aeed7a4eb80d323718b775470fd61be63ab476ff
AlecTech <eremeevoleg@gmail.com>
2020-09-14 21:37:58 -0600

    Add empty folders for action reducer and new component to handle result fields. Add resultlistA.js to action folder

---
+++ src/actions/resultlistA.js
@@ -0,0 +1,35 @@
+const viewResults = resultContent => {
+    return{
+        // This is the NAME of our action.
+      // "type" is how our 'reducer' knows what we want to do.
+      type:'VIEW_FIELD',
+        // This is the DATA we are passing through to the reducer...
+      // so that IT can to ITS JOB!
+      payload: resultContent
+    };
+}
+export { viewResults};
+
+// const addNewToDo = toDoContent => {
+//     return {
+      
+//       type: 'ADD_NEW_TO_DO',
+    
+//       payload: toDoContent
+//     };
+//   }
+  
+  // Another action: remove a to-do!
+  // This time we don't need content - the To-Do already exists!
+  // We DO need to target it somehow though... so an ID will do the trick.
+
+//   const removeToDo = id => {
+//     return {
+//       type: 'REMOVE_TO_DO',
+//       payload: id
+//     };
+//   }
+  
+  // If we want to use this in other files, we must export.
+  // !!! Curly braces let us, comma separated, add multiple exports.
+//   export { addNewToDo, removeToDo };
